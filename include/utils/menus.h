9a43e2ea25a86091c68d39b8a5aaf5e0d6a8636a
Artem Miroshnychenko <coorsepon.icho34@gmail.com>
2023-12-27 00:29:31 +0200

    Release

--- include/utils/menus.h
+++ include/utils/menus.h
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
 #include <variant>
-#include <hardware.h>
+#include "C:\Users\artem\hwinfo_CS50\include\hardware\hardware.h"
 using namespace std;
 using namespace hardware;
 
@@ -35,12 +35,10 @@ menu SetMenu(string name, vector<linkMenu> l)
     return ret;
 }
 
-
 vector<string> start_info{"WELCOME",
-                          "Hardware info is aplication created for Hardward CS50 2023 by Miroshnychenko Artem.",
-                          "Aplication outputs hardware information about your computer. You can select menu point",
-                          "on the left-side by typing number of it. To get instructions how to use program, enter",
-                          "1 that gets you to HELP menu."};
+                          "This aplication created for Hardward CS50 2023 by Miroshnychenko Artem.",
+                          "Aplication outputs hardware information about your computer. To get instructions how",
+                          "to use program, enter HELP."};
 
 BaseBoard bb;
 vector<string> bbInfo = {
@@ -59,24 +57,23 @@ vector<string> cpuInfo = {
     "Maximal clock speed: " + to_string(cpu.maxClockSpeed) + "MHz",
     "Chache size: " + to_string(cpu.cacheSize)};
 
-
 Drives drives;
 vector<string> SetUpDisk()
 {
     vector<string> diskT;
     for (int i = 0; i < drives.disks.size(); i++)
     {
-        diskT.push_back("Device ID: " + drives.disks[i].deviceID);   
-        diskT.push_back("Model: " + drives.disks[i].model);   
-        diskT.push_back("Serial number: " + drives.disks[i].serialN);   
-        diskT.push_back("Size: " + to_string(drives.disks[i].size));   
-        diskT.push_back("Bus type: " + drives.disks[i].busType);   
-        diskT.push_back("Media type: " + drives.disks[i].mediaType);   
+        diskT.push_back("Device ID: " + drives.disks[i].deviceID);
+        diskT.push_back("Model: " + drives.disks[i].model);
+        diskT.push_back("Serial number: " + drives.disks[i].serialN);
+        diskT.push_back("Size: " + to_string(drives.disks[i].size));
+        diskT.push_back("Bus type: " + drives.disks[i].busType);
+        diskT.push_back("Media type: " + drives.disks[i].mediaType);
         diskT.push_back("");
 
         for (int a = 0; a < drives.disks[i].partions.size(); a++)
         {
-            Drives::Disk::Partions p =  drives.disks[i].partions[a];
+            Drives::Disk::Partions p = drives.disks[i].partions[a];
             diskT.push_back("PARTION " + to_string(a));
             diskT.push_back("Volume: " + p.volume);
             diskT.push_back("Total space: " + to_string(p.totalSpace));
@@ -94,7 +91,7 @@ GPU gpu;
 vector<string> SetUpGPU()
 {
     vector<string> gpuT;
-    for(int i = 0; i < gpu.driverVersion.size(); i++)
+    for (int i = 0; i < gpu.driverVersion.size(); i++)
     {
         gpuT.push_back("Model: " + gpu.model[i]);
         gpuT.push_back("Manufacturer: " + gpu.vendor[i]);
@@ -116,7 +113,7 @@ vector<string> osInfo = {
 
 RAM ram;
 vector<string> ramInfo = {
-    "Model: " + ram.model, 
+    "Model: " + ram.model,
     "Manufacturer: " + ram.vendor,
     "Capacity: " + to_string(ram.size) + "GB",
     "Speed: " + to_string(ram.speed) + " ns",
@@ -124,7 +121,21 @@ vector<string> ramInfo = {
 
 vector<string> temp;
 
-linkMenu helpM = SetLinkMenu("HELP", temp);
+vector<string> helpInfo = {
+    "Navigation through the program is facilitated by the menu on the left side and various",
+    "commands. To select a menu option, simply type its corresponding number. For example",
+    "to choose Base Board from the start menu, type 1. To navigate between pages, use next",
+    "or past. Typing next will take you to the next page, while past will take you to",
+    "the previous one.",
+    "",
+    "Once you've selected a menu option, the information will be displayed on the right-side",
+    "menu. Simultaneously, a new menu will appear on the left side. If you choose 1, you",
+    "will return to the start. Alternatively, selecting 2 will save the displayed",
+    "information to a log file in the program folder. You can specify the file path by typing",
+    "it, separated by spaces. If the path contains spaces, enclose it in quotation marks.",
+    "For instance: 2 C:\\LogFiles or 2 \"C:\\Log Files\"."};
+
+linkMenu helpM = SetLinkMenu("HELP", helpInfo);
 linkMenu baseBoardM = SetLinkMenu("BASE BOARD", bbInfo);
 linkMenu cpuM = SetLinkMenu("CPU", cpuInfo);
 linkMenu diskM = SetLinkMenu("DISK", diskInfo);
@@ -132,8 +143,7 @@ linkMenu gpuM = SetLinkMenu("GPU", gpuInfo);
 linkMenu osM = SetLinkMenu("OS", osInfo);
 linkMenu ramM = SetLinkMenu("RAM", ramInfo);
 linkMenu allM = SetLinkMenu("OUTPUT ALL", temp);
-linkMenu logM = SetLinkMenu("SAVE LOG", temp);
 linkMenu exitM = SetLinkMenu("EXIT", temp);
 
 // vector<linkMenu> start_links = {helpM, baseBoardM, cpuM, diskM, gpuM, osM, ramM, allM, logM, exitM};
-vector<menu> start_links = {SetMenu("HARDWARE" ,{baseBoardM, cpuM, diskM, gpuM, osM, ramM}), SetMenu("UTILS", {helpM, allM, exitM})};
+vector<menu> start_links = {SetMenu("HARDWARE", {baseBoardM, cpuM, diskM, gpuM, osM, ramM}), SetMenu("UTILS", {helpM, allM, exitM})};
